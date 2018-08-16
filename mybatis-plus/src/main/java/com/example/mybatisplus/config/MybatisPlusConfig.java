f060415ffc7a05660b087a16d7e04003f3c4e788
wanghaiyang <why20104712@aliyun.com>
2018-08-16 09:13:51 +0800

    性能分析插件

--- mybatis-plus/src/main/java/com/example/mybatisplus/config/MybatisPlusConfig.java
+++ mybatis-plus/src/main/java/com/example/mybatisplus/config/MybatisPlusConfig.java
@@ -1,12 +1,14 @@
 package com.example.mybatisplus.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@MapperScan("com.example.mybatisplus.mappers")
+@MapperScan("com.example.mybatisplus.*.mapper")
 public class MybatisPlusConfig {
 
     /**
@@ -16,6 +18,16 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    /**
+     * SQL执行效率插件
+     * 设置 dev test 环境开启
+     */
+    @Bean
+    @Profile({"dev","test"})
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 
 }
