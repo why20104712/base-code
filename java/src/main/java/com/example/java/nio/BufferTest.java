579e08ae98d4ba0795fe28d8605d2ea53c30d7e2
wanghaiyang <why20104712@aliyun.com>
2018-09-17 15:35:58 +0800

    channel测试

--- java/src/main/java/com/example/java/nio/BufferTest.java
+++ java/src/main/java/com/example/java/nio/BufferTest.java
@@ -6,14 +6,42 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 
 /**
- * nio中buffer用了存储数据的
+ * 一、缓冲区（Buffer）：在 Java NIO 中负责数据的存取。缓冲区就是数组。用于存储不同数据类型的数据
  *
+ * 根据数据类型不同（boolean 除外），提供了相应类型的缓冲区：
+ * ByteBuffer
+ * CharBuffer
+ * ShortBuffer
+ * IntBuffer
+ * LongBuffer
+ * FloatBuffer
+ * DoubleBuffer
+ *
+ * 上述缓冲区的管理方式几乎一致，通过 allocate() 获取缓冲区
+ *
+ * 二、缓冲区存取数据的两个核心方法：
+ * put() : 存入数据到缓冲区中
+ * get() : 获取缓冲区中的数据
+ *
+ * 三、缓冲区中的四个核心属性：
+ * capacity : 容量，表示缓冲区中最大存储数据的容量。一旦声明不能改变。
+ * limit : 界限，表示缓冲区中可以操作数据的大小。（limit 后数据不能进行读写）
+ * position : 位置，表示缓冲区中正在操作数据的位置。
+ *
+ * mark : 标记，表示记录当前 position 的位置。可以通过 reset() 恢复到 mark 的位置
+ *
+ * 0 <= mark <= position <= limit <= capacity
+ *
+ * 四、直接缓冲区与非直接缓冲区：
+ * 非直接缓冲区：通过 allocate() 方法分配缓冲区，将缓冲区建立在 JVM 的内存中
+ * 直接缓冲区：通过 allocateDirect() 方法分配直接缓冲区，将缓冲区建立在物理内存中。可以提高效率
  */
 public class BufferTest {
 
 
     @Test
     public void test() {
+        //1. 分配一个指定大小的缓冲区
         ByteBuffer buffer = ByteBuffer.allocate(1024);
 
         System.out.println("================allocate()===================");
@@ -22,6 +50,7 @@ public class BufferTest {
         System.out.println(buffer.position());
 
 
+        //2. 利用 put() 存入数据到缓冲区中
         System.out.println("================put()===================");
         buffer.put("abcde".getBytes());
         System.out.println(buffer.capacity());
@@ -29,6 +58,7 @@ public class BufferTest {
         System.out.println(buffer.position());
 
 
+        //3. 切换读取数据模式
         System.out.println("================flip()===================");
         buffer.flip();
         System.out.println(buffer.capacity());
@@ -36,12 +66,29 @@ public class BufferTest {
         System.out.println(buffer.position());
 
 
+        //4. 利用 get() 读取缓冲区中的数据
         System.out.println("================get()===================");
         buffer.get();
         System.out.println(buffer.capacity());
         System.out.println(buffer.limit());
         System.out.println(buffer.position());
 
+        //5. rewind() : 可重复读
+        System.out.println("================rewind()===================");
+        buffer.rewind();
+        System.out.println(buffer.capacity());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.position());
+
+        //6. clear() : 清空缓冲区. 但是缓冲区中的数据依然存在，但是处于“被遗忘”状态
+        buffer.clear();
+
+        System.out.println("-----------------clear()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        System.out.println((char)buffer.get());
 
     }
 
